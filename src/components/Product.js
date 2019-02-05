56000ed8c49d249bdcadceb90ab3a0e21cfdc064
Zaur Bairamau <bairamau.z@gmail.com>
2019-02-05 09:34:50 +0300

    add Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,72 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h3>hello from product</h3>
-      </div>
+      <ProductWrapper>
+          <div className="image-container">
+            <Link to="/details">
+              <img className="image" src={img} alt={title} />
+            </Link>
+          </div>
+          <button
+              className="add-btn"
+              disabled={inCart}
+              onClick={() => console.log("added", title)}
+            >
+              {inCart === true ? "In Cart" : <i className="fas fa-cart-plus" />}
+            </button>
+          <div className="product-footer">
+            <p>{title}</p>
+            <p>${price}</p>
+          </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div`
+  
+  display: inline-grid;
+  background: #ffffff;
+  border: 2px solid transparent;
+  
+  .image {
+    max-width: 100%;
+  }
+
+  .image-container {
+    overflow: hidden;
+  }
+  
+  .add-btn {
+    padding: 10px;
+  }
+
+  .product-footer {
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: space-between;
+    padding: 0 10px;
+    font-size: 18px;
+  }
+
+  &:hover {
+    border-color:black;
+    transition: 0.4s;
+
+    .product-footer {
+      background: #dddddd;
+      transition: 0.4s;
+    }
+
+    .image {
+      transform: scale(1.3);
+      transition: 1s;
+    }
+  }
+`;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,38 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import styled from "styled-components";
 
-import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
-  state = {
-    items: storeProducts
-  };
   render() {
-    console.log(this.state.items);
     return (
       <React.Fragment>
         <Title title="products" />
+        <ProductConsumer>
+          {store => (
+            <ListWrapper>
+              {store.products.map(product => (
+                <li key={product.id}>
+                  <Product product={product} />
+                </li>
+              ))}
+            </ListWrapper>
+          )}
+        </ProductConsumer>
       </React.Fragment>
     );
   }
 }
+
+const ListWrapper = styled.ul`
+  margin:0;
+  padding:0 0 30px 0;
+  display: grid;
+  grid-gap: 10px;
+  list-style: none;
+  grid-template-columns: repeat(auto-fill, 360px);
+  justify-items:center;
+  justify-content:center;
+`;
